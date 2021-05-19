a27a1c363a46039e3e46b63092f78846dae7c241
Michael Suzzi <michael163377@hotmail.com>
2021-05-20 01:57:19 +1000

    Wk 11 Assessed Lab practice

---
+++ ass2-image_processing.py
@@ -0,0 +1,4 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as img
+import numpy as np
+import scipy.ndimage
---
+++ wk11-assprac.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def f(x):
+    return 3 * np.sin(0.1*x)
+
+
+def trap_function(g, a, b, N):
+    deltax = (b - a) / N
+    mid_sum = 0; trap = 1
+    while trap != N:
+        mid_sum += g(a + deltax * trap)
+        trap += 1
+    return deltax * (mid_sum + (g(a) + g(b)) / 2)
+
+
+print(trap_function(f, 0, np.pi/4, 10))
